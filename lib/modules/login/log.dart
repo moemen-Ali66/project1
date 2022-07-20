2df799f658a986aa60d2cf0f7c63f6ee38d0a662
pc <m>
2022-07-20 06:28:45 +0200

    m

--- lib/modules/login/log.dart
+++ lib/modules/login/log.dart
@@ -1,29 +1,37 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:login/shared/components/components.dart';
+class log extends StatefulWidget {
+ const log({Key? key}) : super(key: key);
 
-class log extends StatelessWidget {
+  @override
+  State<log> createState() => _logState();
+}
+
+class _logState extends State<log> {
   var mycontroller=TextEditingController();
   var mycontrollerpass=TextEditingController();
   var formkey = GlobalKey<FormState>();
-
+  bool chek=true;
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading:const Icon(
+        leading: const Icon(
           Icons.login_outlined,
         ),
-        title:const Text(
+        title: const Text(
           'login',
           style: TextStyle(
             fontSize: 30.0,
           ),
         ),
-        actions:const [
+        actions: const [
           Icon(
             Icons.login_rounded,
-           ),
+          ),
         ],
       ),
       body: SingleChildScrollView(
@@ -47,89 +55,56 @@ class log extends StatelessWidget {
                       ),
                     ),
                     SizedBox(height: 30.0),
-                    Container(
-                      child: TextFormField(
-                        validator:(value){
-                          if(value!.isEmpty){
-                            return 'email must not be embty';
-                          }
-                            return null ;
-                        } ,
-                        controller: mycontroller,
-                        onFieldSubmitted: (val) {
-                          print(val);
-                        },
-                        onChanged: (val) {
-                          print(val);
-                        },
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.email_sharp),
-                          label: Text('E-mail'),
-                          labelStyle: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w800),
-                          hintText: 'writeE-mail',
-                          hintStyle: TextStyle(backgroundColor: Colors.white10),
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
+                    default_Form(
+                      control: mycontroller,
+                      keyboard: TextInputType.emailAddress,
+                      label: 'E_mail address',
+                    hinttext: 'write e_mail address',
+                    icon:Icon(Icons.email_outlined) ,
+                      validate: (value){
+                        if (value.isEmpty){
+                          return 'email is empty';
+                        }
+                        return null;
+                      }
                     ),
+
                     const SizedBox(height: 30.0),
-                    Container(
-                      child: TextFormField(
-                        validator:(value){
-                          if(value!.isEmpty){
-                            return 'password must not be embty';
+                    default_Form(control: mycontrollerpass,
+                        keyboard: TextInputType.visiblePassword,
+                        label: 'password',
+                        hinttext: 'write password',
+                        icon:Icon(Icons.lock) ,
+                        suficon: Icon(Icons.remove_red_eye_outlined),
+                        validate: (value){
+                          if (value.isEmpty){
+                            return 'password is empty';
                           }
-                          return null ;
-                        } ,
-                        controller: mycontrollerpass,
-                        keyboardType: TextInputType.visiblePassword,
-                        onChanged: (val) {
-                          print(val);
-                        },
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          suffixIcon: Icon(Icons.visibility),
-                          prefixIcon: Icon(Icons.lock),
-                          label: Text('password'),
-                          labelStyle: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w800),
-                          hintText: 'Entre password',
-                          hintStyle: TextStyle(backgroundColor: Colors.white10),
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
+                          return null;
+                        }),
                     SizedBox(height: 30.0),
                     default_button(
-                      background: Colors.blue,
-                      function:(){
-                        if (formkey.currentState!.validate()){
-                          print(mycontroller.text);
-                          print(mycontrollerpass.text);
-                        }
-
-                    },
+                          function: () {
+                            if (formkey.currentState!.validate()) {
+                              print(mycontroller.text);
+                              print(mycontrollerpass.text);
+                            }
+                          },
                       text: 'login',
-                      width: double.infinity,
-                    ),
+                         ),
                     SizedBox(height: 10.0,),
-
                     Container(
-
                       child:
-
                       Row(children: [
-
-                        Text('Don\'t have anaccount?',style: TextStyle(color: Colors.blue),),
+                        Text('Don\'t have anaccount?',
+                          style: TextStyle(color: Colors.blue),),
                         SizedBox(width: 10.0,),
-                        TextButton(onPressed: (){}, child: const Text('Registere',
-                          style: TextStyle(backgroundColor: Colors.blue,color:Colors.white,fontWeight:FontWeight.bold,),),)
+                        TextButton(onPressed: () {},
+                          child: const Text('Registere',
+                            style: TextStyle(
+                              backgroundColor: Colors.blue,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,),),)
                       ],),
                     ),
 
@@ -143,3 +118,6 @@ class log extends StatelessWidget {
     );
   }
 }
+
+
+
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,47 +1,47 @@
 import 'package:flutter/material.dart';
 
 Widget default_button({
-   required double width,
-   required Color background,
-   required Function function,
+  double width=double.infinity,
+    Color background=Colors.blue,
+   required  function,
    required String text ,
 }) =>Container(
    width: width,
    color:background,
 child: MaterialButton(
-onPressed: function() ,
-child: Text(text),
+onPressed: function ,
+child: Text(text.toUpperCase()),
 textColor: Colors.white,
 ),
 );
 //************************* TextFormFaild ******************************
 Widget default_Form({
-   required TextEditingController CONTROOL,
-   required TextInputType KEYBOARD,
-   Function? change,
-   required Function validate,
-   required String text,
+   required TextEditingController control,
+   required TextInputType keyboard,
+     change,
+   required  validate,
+   required String label,
    required String hinttext,
-   required Widget icon ,
+   required Widget icon,
+   Widget? suficon ,
+   bool boolen= false ,
 }) => Container(
-child: TextFormField(
- controller: CONTROOL,
-onFieldSubmitted: (val) {
-print(val);
-},
-onChanged:change!(),
-   validator:validate() ,
-keyboardType: KEYBOARD,
-decoration:  InputDecoration(
-prefixIcon: icon,
-label: Text(text),
-labelStyle: TextStyle(
-color: Colors.black,
-fontSize: 20,
-fontWeight: FontWeight.w800),
-hintText: hinttext,
-hintStyle: TextStyle(backgroundColor: Colors.white10),
-border: OutlineInputBorder(),
-),
-),
-);
+   child: TextFormField(
+      validator: validate,
+      controller: control,
+      keyboardType: keyboard,
+      onChanged:change,
+      obscureText:boolen,
+      decoration:   InputDecoration(
+         prefixIcon: icon,
+         suffixIcon: suficon,
+         label: Text(label),
+         labelStyle:const TextStyle(
+             color: Colors.black,
+             fontSize: 20,
+             fontWeight: FontWeight.w800),
+         hintText: hinttext ,
+         hintStyle: TextStyle(backgroundColor: Colors.white10),
+         border: OutlineInputBorder(),
+      ),
+   ),);
