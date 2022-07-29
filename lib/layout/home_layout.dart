856f447b568707a9aed3652cf51e0a0a267b7381
pc <m>
2022-07-29 13:49:18 +0200

    m

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:login/modules/Archive%20Tasks/archive%20tasks.dart';
 import 'package:login/modules/Done%20Tasks/done%20tasks.dart';
 import 'package:login/modules/New%20Tasks/new%20tasks.dart';
+// import 'package:sqflite/sqflite.dart';
 
 class home_layout extends StatefulWidget {
   const home_layout({Key? key}) : super(key: key);
@@ -22,15 +23,33 @@ class _home_layoutState extends State<home_layout> {
     'Done_Tasks',
     'archive_Tasks',
   ];
+  var scaffoldkey=GlobalKey<ScaffoldState>();
+  bool? isbottomsheet=false;
+//.  Database? database;
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)  {
     return Scaffold(
+      key: scaffoldkey,
       appBar: AppBar(
         title: Text(title[curentindex]),
       ),
        floatingActionButton:FloatingActionButton(
          child:Icon(Icons.eleven_mp),
-          onPressed: (){},) ,
+          onPressed: ()
+          {
+            if(isbottomsheet!){
+              scaffoldkey.currentState?.showBottomSheet((context) => Container(
+                width: double.infinity,
+                height: 120,
+                color: Colors.red,
+              ));
+            }
+            else{
+              Navigator.pop(context);
+              isbottomsheet=false;
+            }
+
+          },) ,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: curentindex,
         items: [
@@ -47,4 +66,58 @@ class _home_layoutState extends State<home_layout> {
       body:screens[curentindex],
     );
   }
+
+  Future<String> getName() async
+  {
+    return  'ahmed ali';
+  }
+
+ //  void createdatabase()async
+ //  {
+ //      database=await openDatabase(
+ //    'todo.db',
+ //   version: 1,
+ //   onCreate: (database,version){
+ //      print('database created');
+ //      database.execute('CREATE TABLE tasks(id INTEGER PRIMARY KEY,title TEXT,date TEXT,time TEXT,status TEXT ) ').then((value){
+ //        print('table is creating');
+ //      }).catchError((error){
+ //        print('error in creating table${error.toString()}');
+ //      });
+ //   },
+ //   onOpen: (database){
+ //     print('database opened');
+ //
+ //   },
+ //
+ // );
+ //
+ //  }
+ //  void insertdatabase(){
+ //    database!.transaction((txn) {
+ //      txn.rawInsert('INSERT INTO tasks(title,date,time,status) values("first task","2202","12:00","new",)').then((value){
+ //        print('table is inserting');
+ //      }).catchError((error){print('error ${error.toString()}');});
+ //      return null;
+ //    });
+ //  }
 }
+// HADLING ERROR
+// try{
+//   var NAME=await getName();
+//   print (NAME);
+//   print ('NAME');
+//   throw('error !!!!!!!');
+//
+// } catch(error){
+//   print('error ${error.toString()}');
+// }
+//       getName().then((value)
+//       {
+//      print (value);
+//      print ('NAME');
+//      // throw('error !!!!!!!');
+//       }
+//       ).catchError((error){
+//         print('error ${error.toString()}');
+//       });
